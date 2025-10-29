7f4b92b37556ee495c65bcec51dca1a44a685419
Nick Alvais <nickalvais2006@gmail.com>
2025-10-29 14:25:45 -0400

    Trim input and convert to postfix expression

--- Main.java
+++ Main.java
@@ -5,18 +5,59 @@ public class Main{
         boolean stop;
         do{         
             System.out.println("Enter a boolean expression to be evaluated: ");
-            String exprs = input.nextLine();
-            System.out.println(exprs);
+            String exprs = input.nextLine().trim();
+            System.out.println(toPostfix(exprs));
             System.out.println("Would you like to evaluate another boolean expression? (type 'y' to keep going or 'n' to stop): ");
             char ans = input.nextLine().charAt(0);
             stop = (ans == 'y') ? false : true;
         } while (!stop);                                        //Get expression from user
     }
+
+    public static String toPostfix(String exprs){
+        String postfix = "";                                    //Postfix expression
+        ArrayStack<String> opStack = new ArrayStack<>();        //Operations Stack
+        String[] tokens = exprs.split(" ");               //Turn original infix expression to a list in order to iterate
+        for (String token : tokens){                            
+            if (isNumber(token)) {                              //If number, add to postfix expression
+                postfix += (" "+token);                     
+            }
+            else {                                              //If operand
+                if (token == ")") {                             //If closing paranthesis, add all operators to postfix expression until closing paranthesis is found
+                    String top = opStack.peek();
+                    while (top != "("){                         
+                        postfix += (" "+ opStack.pop());
+                        top = opStack.pop();
+                    }
+                }
+                else{                                           //If not parantehsis, add operator to postfix expression
+                    opStack.push(token);
+                }
+            }
+        }
+        if (!opStack.isEmpty()){                                //Add all remaining operators within the operator stack
+            while (!opStack.isEmpty()){
+                postfix += (" "+opStack.pop());
+            }
+        }
+        return postfix;
+    }
+
+    public static boolean isNumber(String token){               //Check if number or operand
+        try {
+            Integer.parseInt(token);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+
+
         //Next steps:
         //1. Create expression evaluation method
         //- split string into list of characters
         //- convert infix expression to postfix notation using a stack and algorithm
         //- create algorithm to evaluate postfix expression
         //- return true or false
-        
+        //2. Check whether input is valid or not
 }
