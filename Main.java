49959d95a36961697704e5cf259e54e2101124d6
Nick Alvais <nickalvais2006@gmail.com>
2025-10-31 19:00:37 +0000

    Operation evaluator

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ public class Main{
         do{         
             System.out.println("Enter a boolean expression to be evaluated: ");
             String exprs = input.nextLine().trim();
-            System.out.println(toPostfix(exprs));
+            System.out.println(Evaluate(toPostfix(exprs)));
             System.out.println("Would you like to evaluate another boolean expression? (type 'y' to keep going or 'n' to stop): ");
             char ans = input.nextLine().charAt(0);
             stop = (ans == 'y') ? false : true;
@@ -16,15 +16,15 @@ public class Main{
     public static String toPostfix(String exprs){
         String postfix = "";                                    //Postfix expression
         ArrayStack<String> opStack = new ArrayStack<>();        //Operations Stack
-        String[] tokens = exprs.split(" ");               //Turn original infix expression to a list in order to iterate
+        String[] tokens = exprs.split("\\s+");            //Turn original infix expression to a list in order to iterate
         for (String token : tokens){                            
             if (isNumber(token)) {                              //If number, add to postfix expression
                 postfix += (" "+token);                     
             }
             else {                                              //If operand
-                if (token == ")") {                             //If closing paranthesis, add all operators to postfix expression until closing paranthesis is found
+                if (token.equals(")")) {                             //If closing paranthesis, add all operators to postfix expression until closing paranthesis is found
                     String top = opStack.peek();
-                    while (top != "("){                         
+                    while (!top.equals("(")){                         
                         postfix += (" "+ opStack.pop());
                         top = opStack.pop();
                     }
@@ -49,8 +49,110 @@ public class Main{
         } catch (NumberFormatException e) {
             return false;
         }
+        
     }
 
+    public static boolean Evaluate(String postfix){
+        ArrayStack<Integer> stack = new ArrayStack<>();
+        ArrayStack<Boolean> boolStack = new ArrayStack<>();
+         String[] tokens = postfix.trim().split("\\s+");
+        for (String token : tokens) {
+            if (isNumber(token)) {
+                int num = Integer.parseInt(token);
+                stack.push(token);
+
+
+        }
+        return true;
+        
+        
+    }
+
+    public static boolean evaluateOperator(ArrayStack<Integer> stack, ArrayStack<Boolean> boolStack, String operator) {
+        if ((stack.size() < 2) || (boolStack.size() < 2)) {
+            System.out.println(" (Not enough operands)");
+        return false;
+        }
+        if (operator.equals("!")){
+            boolStack.push(!boolStack.pop());
+        }
+        else{
+            boolean op2 = boolStack.pop();
+            boolean op1 = boolStack.pop();
+            if (operator.equals("&&")){
+                boolStack.push(op1 && op2);
+                return true;
+            }
+            else if (operator.equals("||")){
+                boolStack.push(op1 || op2);
+                return true;
+            }
+            else {
+                System.out.println("Illegal operator");
+                return false;
+            }
+        }
+
+        int op2 = stack.pop();
+        int op1 = stack.pop();
+        
+
+        if (operator.equals("<")) {
+            boolStack.push(op1 < op2);
+            return true;
+        }
+        else if (operator.equals(">")) {
+            boolStack.push(op1 > op2);
+            return true;
+        }
+        else if (operator.equals("<=")) {
+            boolStack.push(op1 <= op2);
+            return true;
+        }
+        else if (operator.equals(">=")) {
+            boolStack.push(op1 >= op2);
+            return true;
+        }
+        else if (operator.equals("==")) {
+            boolStack.push(op1 >= op2);
+            return true;
+        }
+        else if (operator.equals("!=")) {
+            boolStack.push(op1 != op2);
+            return true;
+        }
+        else {
+            System.out.println(" (Illegal operator)");
+            return false;
+        }
+}
+        
+    public static boolean evaluateOperation(ArrayStack<Boolean> boolStack, String operator){
+        if (boolStack.size() < 2) {
+            System.out.println("(Not enough operands)");
+            return false;
+        }
+        if (operator.equals("!")){
+            boolStack.push(!boolStack.pop());
+        }
+        else{
+            boolean op2 = boolStack.pop();
+            boolean op1 = boolStack.pop();
+            if (operator.equals("&&")){
+                boolStack.push(op1 && op2);
+                return true;
+            }
+            else if (operator.equals("||")){
+                boolStack.push(op1 || op2);
+                return true;
+            }
+            else {
+                System.out.println("Illegal operator");
+                return false;
+            }
+        }
+        return false;
+    }
 
 
         //Next steps:
@@ -60,4 +162,5 @@ public class Main{
         //- create algorithm to evaluate postfix expression
         //- return true or false
         //2. Check whether input is valid or not
+}
 }
